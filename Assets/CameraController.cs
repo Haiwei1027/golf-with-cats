2ed3c183c8620caa86751237eea9e7d3465e5e25
Garance AR <79841946+TheWonderfulLizardOfOz@users.noreply.github.com>
2023-06-16 14:59:40 +0100

    Added panning around with scroll wheel

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    private Vector3 prevMousePos;
+    [SerializeField] private Transform freeLookTarget;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        prevMousePos = Vector3.zero;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(2))
+        {
+            GetComponent<CameraFollow>().followTarget = freeLookTarget;
+        }
         
+        if (Input.GetMouseButton(2))
+        {   
+            Vector3 mouseChange = Vector3.zero;
+            mouseChange.x = prevMousePos.x - Input.mousePosition.x;
+            mouseChange.z = prevMousePos.y - Input.mousePosition.y;
+            freeLookTarget.position = freeLookTarget.position + mouseChange*0.01f;
+        }
+
+        prevMousePos = Input.mousePosition;
     }
 }
